5c9fb11db5e1c85fd4e2be2582da646d1bad1816
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-16 09:47:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ end
     song
   end
   
-  def self.create_from_filename
+  def self.create_from_filename(filename)
     song = self.create
     split_filename = filename.chomp(".mp3").split(" – ")
     song.name = split_filename[1]
